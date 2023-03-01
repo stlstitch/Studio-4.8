40cb7f7d736e8ad8fe1106bfa7eb10ae0e8f3e48
stlstitch <jamesbm1983@gmail.com>
2023-03-01 03:46:44 -0600

    1st version

---
+++ src/Menu.java
@@ -0,0 +1,43 @@
+public class Menu {
+
+    public String getStarters() {
+        return starters;
+    }
+
+    public void setStarters(String starters) {
+        this.starters = starters;
+    }
+
+    public String getMainCourse() {
+        return mainCourse;
+    }
+
+    public void setMainCourse(String mainCourse) {
+        this.mainCourse = mainCourse;
+    }
+
+    public String getFinishers() {
+        return finishers;
+    }
+
+    public void setFinishers(String finishers) {
+        this.finishers = finishers;
+    }
+
+    public String getDrinks() {
+        return drinks;
+    }
+
+    public void setDrinks(String drinks) {
+        this.drinks = drinks;
+    }
+
+    public String starters;
+
+    public String mainCourse;
+
+    public String finishers;
+
+    public String drinks;
+
+}
---
+++ src/MenuItem.java
@@ -0,0 +1,30 @@
+public abstract class MenuItem {
+
+    public MenuItem() {
+    }
+
+    public abstract void menuItem("Mozzarella Sticks");
+
+    public abstract void menuItem("nachos");
+
+    public abstract void menuItem("Garlic Bread");
+
+    public abstract void menuItem("T-Bone Steak");
+
+    public abstract void menuItem ("Rib-Eye Steak");
+
+    public abstract void menuItem ("Fettuccine Alfredo");
+
+    public abstract void menuItem ("Hamburger");
+
+    public abstract void menuItem ("Cheeseburger");
+
+    public abstract void menuItem ("Chicken Strips");
+
+    public abstract void menuItem ("Cheesecake");
+
+    public abstract void menuItem ("Chocolate Lava Cake");
+
+    public abstract void menuItem ("Ice Cream");
+
+}
